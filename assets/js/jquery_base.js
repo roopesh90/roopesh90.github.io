b96a9072a964b637f7c41bf4e892e0d7f16c6880
roopesh90 <roopesh90nair@live.com>
2015-12-01 12:59:38 +0530

    history puststae added to mod urls

--- assets/js/jquery_base.js
+++ assets/js/jquery_base.js
@@ -6,6 +6,14 @@ $(document).ready(function(){
     $('#vCard_navTab a').click(function (e) {
       e.preventDefault()
       $(this).tab('show')
+      if ($(this).attr('href') == "#about"){
+          _loc= window.location.origin = window.location.pathname;
+          history.pushState({}, "", _loc);
+      }
+      else{
+          history.pushState({}, "", this.href);
+      }
+      
     });
     
     //To switch tabs if url has nav button hash
@@ -16,7 +24,6 @@ $(document).ready(function(){
             _ancr_href = "#"+_hsh_array[1];
             _ancr = $('#vCard_navTab a[href="' + _ancr_href + '"]');
             $( _ancr ).trigger( "click" );
-            console.log(_ancr);
         }
     };
     check_n_switchTab();
