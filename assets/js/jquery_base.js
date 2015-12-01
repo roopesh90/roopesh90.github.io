4e19df3a43982cc11c1a849f9d4521d694abe5e9
roopesh90 <roopesh90nair@live.com>
2015-12-01 12:25:02 +0530

    duocument ready added, tab name changed, js added to switch tab as per url hash

--- assets/js/jquery_base.js
+++ assets/js/jquery_base.js
@@ -1,5 +1,20 @@
+/*
+*Cutom js for site functioning
+*/
+$(document).ready(function(){ 
+    $('#vCard_navTab a').click(function (e) {
+      e.preventDefault()
+      $(this).tab('show')
+    });
 
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+    var check_hash = function(){
+        var _hsh = window.location.hash;
+        var _hsh_array = _hsh.split(/[#?|&]/);
+        if (_hsh_array.length > 1){
+            _ancr_href = "#"+_hsh_array[1];
+            _ancr = $('#vCard_navTab a[href="' + _ancr_href + '"]');
+            $( _ancr ).trigger( "click" );
+            console.log(_ancr);
+        }
+    };
 });
