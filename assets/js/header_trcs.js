f6d7436342313bd72c6762c589e5ac2b42898374
roopesh90 <roopesh90nair@live.com>
2015-12-02 20:51:59 +0530

    fb share moved to html

--- assets/js/header_trcs.js
+++ assets/js/header_trcs.js
@@ -9,14 +9,5 @@
   ga('send', 'pageview');
 
 
-//Facebook JS JDK
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 // Twitter JS 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
